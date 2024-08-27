897a37808087038dfa099814128a2cfe43330ea8
jgull <javidgull@gmail.com>
2024-08-27 05:43:32 -0400

    Adding security patch to the code main.c in the branch master

--- main.c
+++ main.c
@@ -1,15 +1,17 @@
 pseudo code
 {
-Initial Function()
-{
-  //Sample Code
-}
+   Initial Function()
+     { 
+       //Sample Code
+     }   
 
-Initial Security()
-{
- //Sample Code
-}
+    Initial Security()
+     {
+       //Sample Code
+     }
 
-
-
+     Port Security()
+     {
+       //Adding Security to the ports
+     }
 }
