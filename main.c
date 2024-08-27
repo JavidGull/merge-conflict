4ea6e10e0a96cdd5757162453de83354be0c3093
jgull <javidgull@gmail.com>
2024-08-27 06:02:47 -0400

    Adding feature2 patch to the code main.c in the branch feature2

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 pseudo code
 {
-Initial Function()
-{
-  //Sample Code
-}
+   Initial Function()
+    {
+      //Sample Code
+    }
 
-Initial Security()
-{
- //Sample Code
-}
+    Initial Security()
+    {
+      //Sample Code
+    }
 
-
+    Feature2 Function ()
+    {
+      //Adding Audio feature to the main.c code in branch feature2
+    }
 
 }
